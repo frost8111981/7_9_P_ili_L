fa84f873329ef8a6448be21252914dc5f361ac1f
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-02-19 13:55:35 +0300

    Посл изменение last

--- src/com/frost/Main.java
+++ src/com/frost/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
 
@@ -11,10 +12,12 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         ArrayList<String> strings = new ArrayList<String>();
-        strings.add("роза");  // 1. удалять из списка строк все слова, содержащие букву "р"
+        strings.add("роза");  // 1. удалять букву "р"
+        strings.add("мера");  // 1. удалять букву "р"
         strings.add("лоза");  // 2. удваивать все слова содержащие букву "л"
-        strings.add("лира");  // 3. если слово содержит и букву "р" и букву "л", то оставить это слово без изменений
-
+        strings.add("лира");  // 3. "р" и букву "л", то оставить это слово без изменений
+        strings.add("вода");  // не трогать
+        strings.add("упор");  // 1. удалять букву "р"
         strings = fix(strings);
 
         for (int i = 0; i < strings.size(); i++) {
@@ -23,18 +26,18 @@ public class Main {
     }
 
     public static ArrayList<String> fix(ArrayList<String> strings) {
-        for (int i = 0; i < strings.size(); i++) {
-            if (strings.get(i).contains("р")) {
-                if (strings.get(i).contains("р") && strings.get(i).contains("л")) {
-                } else {
-                    strings.remove(strings.get(i));
-                }
+        Iterator iterator = strings.iterator();
+        while (iterator.hasNext()) {
+            String item = (String) iterator.next();
+            if (item.contains("р") && !(item.contains("л"))) {
+                iterator.remove();
             }
         }
         for(int i = 0; i < strings.size() - 1; i ++){
             if(strings.get(i).contains("л") && !(strings.get(i).contains("р"))){
                 strings.add(strings.get(i));
             }
+
         }
 
         return strings;
