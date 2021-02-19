0ec4f3d31a57964e673aa8471f4982a887180d1f
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-02-19 10:12:22 +0300

    Посл изменение

--- src/com/frost/Main.java
+++ src/com/frost/Main.java
@@ -11,24 +11,33 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         ArrayList<String> strings = new ArrayList<String>();
-        strings.add("роза");
-        strings.add("лоза");
-        strings.add("лира");
+        strings.add("роза");  // 1. удалять из списка строк все слова, содержащие букву "р"
+        strings.add("лоза");  // 2. удваивать все слова содержащие букву "л"
+        strings.add("лира");  // 3. если слово содержит и букву "р" и букву "л", то оставить это слово без изменений
 
-//        strings = fix(strings);
-        fix(strings);
+        strings = fix(strings);
+
+        for (int i = 0; i < strings.size(); i++) {
+            System.out.println(strings.get(i));
+        }
     }
 
     public static ArrayList<String> fix(ArrayList<String> strings) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < strings.size(); i++) {
             if (strings.get(i).contains("р")) {
-                strings.remove(strings.get(i));
-                System.out.println("if");
+                if (strings.get(i).contains("р") && strings.get(i).contains("л")) {
+                } else {
+                    strings.remove(strings.get(i));
+                }
             }
-            System.out.println("for");
         }
-        return strings;
+        for(int i = 0; i < strings.size() - 1; i ++){
+            if(strings.get(i).contains("л") && !(strings.get(i).contains("р"))){
+                strings.add(strings.get(i));
+            }
+        }
 
+        return strings;
     }
 }
 
